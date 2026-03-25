3b25b81d97089091e28a8ef402ff5f8c052ae738
Asare Prosper Shaibu <asareprosper143@gmail.com>
2026-03-25 13:43:10 +0000

    feat: implement NextAuth authentication with Google and Credentials providers, and configure Axios for API communication with an updated Capacitor server URL.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'MindBridge',
   webDir: 'out',
   server: {
-    // url: 'http://192.168.0.197:3000', 
+    url: 'http://10.12.56.68:3000', 
     cleartext: true
   }
 };
